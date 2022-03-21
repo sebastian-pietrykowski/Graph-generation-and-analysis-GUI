ef87d3d81ac5ef4038a991930e49b859e7fb5f97
Sebastian Pietrykowski <sebastian.pietrykowski.stud@pw.edu.pl>
2022-03-21 16:53:21 +0100

    Dodanie komentarzy do funkcji w dijkstra.h

--- projekt1-C/Kod/dijkstra.h
+++ projekt1-C/Kod/dijkstra.h
@@ -12,7 +12,7 @@ typedef struct {
 
 } * PriorityQueue;
 
-// contains array storing vertexes which changes its size when necessary
+// Contains array storing vertexes which changes its size when necessary.
 typedef struct {
 
 	int * vertexes;   // array of vertexes
@@ -20,17 +20,44 @@ typedef struct {
 
 } * Set;
 
+// Removes from PriorityQueue pp vertex with the shortest distance, returns it.
 int PP_get( PriorityQueue pp );
+
+/* Adds to PriorityQueue pp vertex with given number (vertex) and distance.
+ * If PriorityQueue pp is NULL, then initialize it. */
 void PP_put( PriorityQueue pp, int vertex, double distance );
 
+
+/* Adds to Set set vertex with given number (vertex).
+ * If Set set is NULL, then initialize it. */
 void Set_add( Set set, int vertex );
 
-int initiate_values_dijkstra( graph_t graph, int start_vertex_number, int ** predecessors, int ** distances );
-void relax( graph_t graph, int start_vertex_number, int vertex1, int vertex2, int ** predecessors, int ** distances );
-int * dijkstra( graph_t graph, int start_vertex_number );
-void print_path( int * predecessors, int start_vertex_number, int end_vertex_number );
 
+//Initializes arrays predecessors[] and distances[], following the Dijkstra's algorithm.
+int initiate_values_dijkstra( graph_t graph, int start_vertex_number, int ** predecessors, int ** distances );
+
+/* If specific conditions are true, then add vertex2 to arrays
+ * predecessors[] and distances[], following the Dijkstra's algorithm. */
+void relax( graph_t graph, int start_vertex_number, int vertex1, int vertex2, int ** predecessors, int ** distances );
+
+/* Function responsible for performing Dijkstra's algorithm.
+ * Returns array where element with given index representing number of praticular vertex
+ * stores number of vertex beeing its predecessor. */
+int * dijkstra( graph_t graph, int start_vertex_number );
+
+/* Given array of predecessors received from dijkstra(...) function,
+ * prints the shortes path from vertex with number start_vertex_number
+ * to vertex with number end_vertex_number.
+ * 
+ * If does_print_wages is set to 1, then print weights of edges the path passes,
+ * if it is set to 0, then doesn't print these weights. */
+void print_path( int * predecessors, int start_vertex_number, int end_vertex_number, int does_print_weights );
+
+
+// Remove PriorityQueue pp from memory.
 void free_PP( PriorityQueue pp );
+
+// Remove Set set from memory.
 void free_Set( Set set );
 
 #endif
