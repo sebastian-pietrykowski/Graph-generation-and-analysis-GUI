d5efa3d9c1187bede5d53cbdaf436e4d6e09a9a3
Pawel Borkowski <pawel.borkowski4.stud@pw.edu.pl>
2022-03-26 18:06:57 +0100

    Plik naglowkowy dla algorytmu bfs

---
+++ projekt1-C/Kod/bfs.h
@@ -0,0 +1,23 @@
+#ifndef BFS_H
+#define BFS_H
+
+#include "graph.h"
+
+typedef struct {
+  int * vertexes; /* Array of vertexes in queue */
+  int head; /* The variable head is the index of the vertex in the adjacency matrix for which neighbors will be searched for */
+  int back; /* The variable back is the index of the last vertex in the FIFO queque */
+}* FIFO_t;
+
+int FIFO_get(FIFO_t fifo); /* Function responsible for removing the vertex from FIFO queue in order to find his neighbors */
+
+void FIFO_put(FIFO_t fifo, int vertex); /* Function responsible for putting the vertex in FIFO queue */
+
+int bfs(graph_t graph, int start_vertex_number); /* Main function of the bfs algorithm */
+
+void free_bfs(FIFO_t fifo, int * visited); /* Memory clean up */
+
+int is_FIFO_empty(FIFO_t fifo); /* Function checks if there are vertexes in the queue that are wating to be checked for having neighbors */
+
+#endif
+
