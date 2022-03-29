26e168c95c7e91bf1d20008ea7931deb6bbfa511
Sebastian Pietrykowski <sebastian.pietrykowski.stud@pw.edu.pl>
2022-03-29 20:52:17 +0200

    Dodanie funkcji generate_random_graph(...)

--- projekt1-C/Kod/generator.c
+++ projekt1-C/Kod/generator.c
@@ -19,13 +19,28 @@ graph_t generate_random_graph( int columns, int rows, double from_weight, double
 	int number_of_vertices = columsn * rows;
 
 	srand( time( NULL ) );
-	int max_number_of_edges = -1;
-	do {
-		max_number_of_edges = rand() % number_of_vertices * 1.05;
-	} while( max_number_of_edges != 0 );
 
-	for( int vertex_from_counter = 0; vertex_from_counter < number_of_vertices; vertex_from_counter ) {
-		
+	// generate random number of generated edges
+	int max_number_of_edges = 0;
+	while( max_number_of_edges == 0 ) {
+		max_number_of_edges = rand() % number_of_vertices;
 	}
 
+	for( int vertex_from_counter = 0; vertex_from_counter < max_number_of_edges; vertex_from_counter++ ) {
+		
+		// download array of vertices where edges can lead
+		int *potential_neighbors = potential_neighbors( graph, vertex_from_counter );
+		
+		// find random vertex where current edge can lead
+		int vertex_to = -1;
+		while( vertex_to == -1 ) {
+
+			int vertex_to_in_array = rand() % 4;
+			vertex_to = potential_neighbors[ vertex_to_in_array ];
+		}
+		// add edge to graph
+		graph->adj_mat[vertex_from_counter][vertex_to] = from_weight + ( rand() % (to_weight-from_weight+1) );
+		
+		free( potential_neighbors );
+	}
 }
