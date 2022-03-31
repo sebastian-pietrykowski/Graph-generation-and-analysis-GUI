6e80d6f689dcc962b930e0e67bca4bbba53b170f
Pawel Borkowski <pawel.borkowski4.stud@pw.edu.pl>
2022-03-31 20:23:45 +0200

    poprawa

--- projekt1-C/Kod/graph.h
+++ projekt1-C/Kod/graph.h
@@ -9,13 +9,13 @@ typedef struct {
   double **adj_mat; /* adjacency matrix stores information about weights between individual vertives */
 } * graph_t;
 
-graph_t read_graph(FILE *in); /* Function responsible for reading the graph from the input file and creating adjacency matrix */
+graph_t read_graph(FILE *in, graph_t graph); /* Function responsible for reading the graph from the input file and creating adjacency matrix */
 
 int does_have_all_edges(graph_t graph); /* Function responsible for checking if all the possible edges in the graph exist */
 
 int write_graph(graph_t graph, FILE *out); /* Writes the graph structure to the out file */
 
-graph_t make_graph(int columns, int rows); /* Function responsible for initialization graph structure and memory allocation for adj_mat */
+graph_t make_graph(int columns, int rows, graph_t graph); /* Function responsible for initialization graph structure and memory allocation for adj_mat */
 
 int *neighbors(graph_t graph, int vertex); /* Function returns an array of vertices for which there is an edge connecting them to the selected vertex */
 
