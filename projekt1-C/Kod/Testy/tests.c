8d3a75b47d739d88c72a37459edaa65c14118d64
Sebastian Pietrykowski <sebastian.pietrykowski.stud@pw.edu.pl>
2022-04-02 20:28:25 +0200

    Próba dodania Makefile

---
+++ projekt1-C/Kod/Testy/tests.c
@@ -0,0 +1,9 @@
+#include "../data_structures.h"
+#include "tests_data_structures.h"
+
+
+int main() {
+	test_PQ();
+
+	return 0;
+}
--- projekt1-C/Kod/data_structures.c
+++ projekt1-C/Kod/data_structures.c
@@ -140,9 +140,9 @@ int Set_pop( Set set ) {
 
 	// There is no element to be taken from set
 	if( set->no_elements < 1 ) {
-		printf( stderr, "Set.c: Set_pop(...) - There is no element to be taken from Set\n" );
-		System.exit(1);
-		return;
+		fprintf( stderr, "Set.c: Set_pop(...) - There is no element to be taken from Set\n" );
+		exit(1);
+		return 0;
 	}
 	// There is only one element in set
 	else if( set->no_elements == 1 ) {
@@ -163,10 +163,10 @@ int Set_pop( Set set ) {
 			set->elements[ index-1 ] = set->elements[ index ];
 		}
 		set->no_elements--;
-		set->elements = realloc( set->elements, sizeof set->element[0] * set->no_elements );
+		set->elements = realloc( set->elements, sizeof set->elements[0] * set->no_elements );
 		return taken_element_value;
 	}
-	return;
+	return 0;
 }
 
 void free_Set( Set set ) {
