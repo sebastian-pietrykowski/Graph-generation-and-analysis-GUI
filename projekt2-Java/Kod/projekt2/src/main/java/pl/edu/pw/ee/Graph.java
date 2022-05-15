db9ce5b6cb78fa04761599d269a933df0c688382
Sebastian Pietrykowski <sebastian.pietrykowski.stud@pw.edu.pl>
2022-05-15 13:07:15 +0200

    RandomGraphGenerator i CompleteGraphGenerator

--- projekt2-Java/Kod/projekt2/src/main/java/pl/edu/pw/ee/Graph.java
+++ projekt2-Java/Kod/projekt2/src/main/java/pl/edu/pw/ee/Graph.java
@@ -1,16 +1,27 @@
 package pl.edu.pw.ee;
 
 import java.util.LinkedList;
+import java.util.Set;
 
 public class Graph {
     int columns, rows;
     private LinkedList<Edge> adjList[];
-        public Graph( int columns, int rows ) {
-            this.columns = columns;
-            this.rows = rows;
-        }
-        public int getNumberOfVertices() {return 1;}
-        public int getColumns() {return columns;}
-        public int getRows() {return rows;}
-        public void addEdge( Edge e ) {}
+
+    public Graph( int columns, int rows ) {
+        this.columns = columns;
+        this.rows = rows;
+    }
+
+    public int getColumns() {return columns;}
+    public int getRows() {return rows;}
+    public int getNumberOfVertices() {return 1;}
+    
+    public void addEdge( Edge edge ) {}
+    public void removeEdge( int index ) {}
+    public Edge getEdge( int index ) {return null;}
+    public int getNumberOfEdges() {return 1;}
+
+    public Set<Integer> potenialNeighbors( int vertex ) {return null;}
+
+    public int maxPossibleNumberOfEdges() {return 1;}
 }
