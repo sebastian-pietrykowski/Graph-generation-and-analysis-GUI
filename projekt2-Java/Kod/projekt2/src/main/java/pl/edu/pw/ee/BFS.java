98a564b29d323f293f4685f03c05e201de672322
Pawel Borkowski <pawel.borkowski4.stud@pw.edu.pl>
2022-05-20 21:17:43 +0200

    Dodanie zmian

--- projekt2-Java/Kod/projekt2/src/main/java/pl/edu/pw/ee/BFS.java
+++ projekt2-Java/Kod/projekt2/src/main/java/pl/edu/pw/ee/BFS.java
@@ -1,13 +1,58 @@
 package pl.edu.pw.ee;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BFS {
-    
 
-    public BFS( Graph graph, int startVertexNumber ) {
+    private Graph graph;
+    private final int startVertexNumber;
+    private Queue<Integer> fifo;
 
+    public BFS(Graph graph, int startVertexNumber) {
+        this.graph = graph;
+        this.startVertexNumber = startVertexNumber;
+        fifo = new LinkedList<>();
     }
 
-    public boolean checkConnectivty() {
+    public int getStartVertexNumber() {
+        return startVertexNumber;
+    }
+
+    public boolean checkConnectivty(BFS bfs) {
+
+        boolean[] visited = new boolean[graph.getNumberOfVertices()];
+
+        visited[bfs.getStartVertexNumber()] = true;
+     
+        fifo.add(bfs.getStartVertexNumber());
+        // Object[] adjacencyArray = graph.getAdjacencyList().toArray();
+        while (!fifo.isEmpty()) {
+            int currentVertex = fifo.poll();
+            // System.out.println("curr= "+currentVertex);
+            for (int i = 0; i < graph.getAdjacencyList().size(); i++) {
+                //  int neighbourVertex = graph.getAdjacencyList().get(i).getToVertex();
+                if (currentVertex == graph.getAdjacencyList().get(i).getFromVertex()) {
+                    int neighbourVertex = graph.getAdjacencyList().get(i).getToVertex();
+                 //   System.out.println(graph.getAdjacencyList().get(i).getToVertex());
+                    if (!visited[neighbourVertex]) {
+                        visited[neighbourVertex] = true;
+                        fifo.add(neighbourVertex);
+                    }
+                }
+            }
+        }
+      //  for(int i = 0 ; i < visited.length;i++)
+      //  System.out.println(visited[i]);
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+               // System.out.println("?");
+                return false;
+            }
+           // System.out.println("?");
+            //return true;
+
+        }
         return true;
     }
 }
