34733925fc66f3a935192627cfcc2acf130506a3
Pawel Borkowski <pawel.borkowski4.stud@pw.edu.pl>
2022-05-24 18:32:29 +0200

    Dodanie zmian

--- projekt2-Java/Kod/projekt2/src/main/java/pl/edu/pw/ee/Graph.java
+++ projekt2-Java/Kod/projekt2/src/main/java/pl/edu/pw/ee/Graph.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.File;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Set;
 
@@ -19,10 +20,10 @@ public class Graph {
         adjacencyList = new LinkedList<>();
     }
 
-
     public int getColumns() {
         return columns;
     }
+
     public int getRows() {
         return rows;
     }
@@ -32,21 +33,29 @@ public class Graph {
     }
 
     public void addEdge(Edge edge) {
+        this.getAdjacencyList().add(edge);
     }
 
     public void removeEdge(int index) {
+        this.getAdjacencyList().remove(index);
     }
 
     public Edge getEdge(int index) {
-        return null;
+        return this.getAdjacencyList().get(index);
     }
 
-    public Edge getEdge( int startVertexNumber, int endVertexNumber ) {
+    public Edge getEdge(int startVertexNumber, int endVertexNumber) {
+        for (int i = 0; i < this.getAdjacencyList().size(); i++) {
+            if (this.getAdjacencyList().get(i).getFromVertex() == startVertexNumber
+                    && this.getAdjacencyList().get(i).getToVertex() == endVertexNumber) {
+                return this.getAdjacencyList().get(i);
+            }
+        }
         return null;
     }
 
     public int getNumberOfEdges() {
-        return 1;
+        return this.getAdjacencyList().size();
     }
 
     public static Graph readGraph(File file) throws IOException {
@@ -70,15 +79,15 @@ public class Graph {
                 temp = 1;
 
                 String[] tokens = line.split(":| ");
-               
+
                 for (String token : tokens) {
                     if (token.trim().length() > 0) {
-  
+
                         temp++;
                         if (temp % 2 == 0) {
                             try {
                                 toVertex = Integer.parseInt(token);
-                            } catch (Exception e) {
+                            } catch (NumberFormatException e) {
 
                             }
                             if (toVertex < 0) {
@@ -87,7 +96,7 @@ public class Graph {
                         } else {
                             weight = Double.parseDouble(token);
                             Edge edge = new Edge(lineNumber, toVertex, weight);
-                            graph.adjacencyList.add(edge);
+                            graph.addEdge(edge);
                         }
 
                     }
@@ -112,14 +121,61 @@ public class Graph {
     }
 
     public Set<Integer> potenialNeighbors(int vertex) {
-        return null;
+        Set<Integer> potenialNeighborsSet = new HashSet<>();
+        int neighbor;
+        //neighbor to north
+        if ((neighbor = vertex - this.getColumns()) > 0) {
+            potenialNeighborsSet.add(neighbor);
+        }
+        //neighbor to south
+        if ((neighbor = vertex + this.getColumns()) < this.getNumberOfVertices()) {
+            potenialNeighborsSet.add(neighbor);
+        }
+        //neighbors to west and east
+        //find number of row containing vertex
+        int rowNumber = vertex / this.getColumns();
+
+        //find first and last elements of row
+        int startRowNumber = (rowNumber - 1) * this.getColumns();
+        int endRowNumber = ((rowNumber) * this.getColumns()) - 1;
+
+        //check if neighbor to east can exist
+        if (startRowNumber < vertex) {
+            potenialNeighborsSet.add(vertex - 1);
+        }
+
+        //check if neighbor to west can exist
+        if (vertex < endRowNumber) {
+            potenialNeighborsSet.add(vertex + 1);
+        }
+
+        return potenialNeighborsSet;
+
     }
 
     public int getMaxPossibleNumberOfEdges() {
-        return 1;
-    }
+        /* We multiply by 4 because there are only four vertices that are "corners" of the graph that have a maximum of 2 connections to other vertices,
+    *
+    *  then we add a number that is the product of "(this.getColumns() -2 )" , becuase this is the number of vertices in the first columns that are not corner vertices,
+    *  and "2" because we also consult the last column,
+    *  and "3" because this vertices can be connected to up to three other vertices,
+    *
+    *  then we add a number that is the product of "(this.getRows() -2 )" ,because this is the number of vertives in the first row that are not corner vertices ,
+    *  and "2" because we also consult the last row,
+    *  and "3" beacuse this vertices can be connected to up to three other vertices,
+    *
+    *  then we add a number that is the product of "(this.getRows() - 2 ) * ( this.getColumns() - 2 )" because this is the number of vertices that
+       are not the most distant vertices from the center of the graph - lying on its outer sides
+    *  and "4" because this vertices can be connected to up to four other vertices.
+         */
 
-    public boolean isEdgeSituatedProperly(Edge edge) {
-        return true;
+        return (4 * 2 + (this.getColumns() - 2) * 2 * 3 + (this.getRows() - 2) * 2 * 3 + ((this.getRows() - 2) * (2 - this.getColumns())) * 4);
     }
+    /*
+    public boolean isEdgeSituatedProperly(Edge edge) {
+        for (int i = 0; i < this.getAdjacencyList().size(); i++) {
+            if ( ) 
+            }
+        }
+    }*/
 }
