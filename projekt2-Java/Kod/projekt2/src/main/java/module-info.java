a79622b1fa8af3d0c899f570dddf14b2185fca45
Pawel Borkowski <pawel.borkowski4.stud@pw.edu.pl>
2022-05-25 15:48:32 +0200

    .

--- projekt2-Java/Kod/projekt2/src/main/java/module-info.java
+++ projekt2-Java/Kod/projekt2/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module pl.edu.pw.ee {
     requires transitive javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens pl.edu.pw.ee to javafx.fxml;
     exports pl.edu.pw.ee;
