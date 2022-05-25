6a1bad312e8cdda0e9e84bc8b3332fafe2f960b2
Pawel Borkowski <pawel.borkowski4.stud@pw.edu.pl>
2022-05-25 15:46:29 +0200

    zmiana wymiarow okna

--- projekt2-Java/Kod/projekt2/src/main/java/pl/edu/pw/ee/App.java
+++ projekt2-Java/Kod/projekt2/src/main/java/pl/edu/pw/ee/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("primary.fxml"));
-        scene = new Scene(fxmlLoader.load(), 640, 480);
+        scene = new Scene(fxmlLoader.load(), 680, 720);
 
         PrimaryController controller = fxmlLoader.getController();
         controller.initializeGraphPane();
