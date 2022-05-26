be50469adba0da57f2dfcc186435963cfe78f7eb
Pawel Borkowski <pawel.borkowski4.stud@pw.edu.pl>
2022-05-26 22:26:54 +0200

    dodanie zmian

--- projekt2-Java/Kod/projekt2/src/main/java/pl/edu/pw/ee/App.java
+++ projekt2-Java/Kod/projekt2/src/main/java/pl/edu/pw/ee/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("primary.fxml"));
-        scene = new Scene(fxmlLoader.load(), 710, 710);
+        scene = new Scene(fxmlLoader.load(), 730, 750);
 
         PrimaryController controller = fxmlLoader.getController();
         controller.initializeGraphPane();
