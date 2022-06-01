dc91f87b02b607cc72fa8ed8392a64a4ec16a711
Pawel Borkowski <pawel.borkowski4.stud@pw.edu.pl>
2022-06-01 10:34:49 +0200

    dodanie testow bfs

---
+++ projekt2-Java/Kod/projekt2/src/test/java/pl/edu/pw/ee/BFSTest.java
@@ -0,0 +1,82 @@
+package pl.edu.pw.ee;
+
+import MyExceptions.IllegalVertexException;
+import MyExceptions.IllegalWeightException;
+import java.io.File;
+import java.io.IOException;
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BFSTest {
+
+    static String folderAbsolutePath;
+    static String[] bfsTestFiles;
+
+    @BeforeClass
+    public static void initiateBFS() throws IOException {
+        folderAbsolutePath = new File("src\\graphs\\test graphs\\BFS").getAbsolutePath();
+
+        bfsTestFiles = new String[4];
+
+        // 42x42 random
+        bfsTestFiles[0] = folderAbsolutePath + "\\randomNotConnectedGraph.txt";
+        // 47x39 connected
+        bfsTestFiles[1] = folderAbsolutePath + "\\connectedGraph.txt";
+        // 19x26 complete
+        bfsTestFiles[2] = folderAbsolutePath + "\\completeGraph.txt";
+        // 123x101 random
+        bfsTestFiles[3] = folderAbsolutePath + "\\random2NotConnectedGraph.txt";
+    }
+
+    @Test
+    public void testBFSfor42x42Graph() {
+        try {
+            File testFile = new File(bfsTestFiles[0]);
+            Graph graph = Graph.readGraph(testFile);
+            BFS bfs = new BFS(graph, 0);
+            assertEquals(bfs.getStartVertexNumber(), 0); 
+            assertEquals(bfs.checkConnectivty(), false);
+        } catch (IOException | IllegalVertexException | IllegalWeightException e) {
+            System.out.println("Niepoprawny plik");
+        }
+    }
+
+    @Test
+    public void testBFSfor47x39Graph() {
+        try {
+            File testFile = new File(bfsTestFiles[1]);
+            Graph graph = Graph.readGraph(testFile);
+            BFS bfs = new BFS(graph, 0);
+            assertEquals(bfs.getStartVertexNumber(), 0); 
+            assertEquals(bfs.checkConnectivty(), true);
+        } catch (IOException | IllegalVertexException | IllegalWeightException e) {
+            System.out.println("Niepoprawny plik");
+        }
+    }
+
+    @Test
+    public void testBFSfor19x26Graph() {
+        try {
+            File testFile = new File(bfsTestFiles[2]);
+            Graph graph = Graph.readGraph(testFile);
+            BFS bfs = new BFS(graph, 0);
+            assertEquals(bfs.checkConnectivty(), true); 
+        } catch (IOException | IllegalVertexException | IllegalWeightException e) {
+            System.out.println("Niepoprawny plik");
+        }
+    }
+
+    @Test
+    public void testBFSfor123x101Graph() {
+        try {
+            File testFile = new File(bfsTestFiles[3]);
+            Graph graph = Graph.readGraph(testFile);
+            BFS bfs = new BFS(graph, 0);
+            assertEquals(bfs.getStartVertexNumber(), 0); 
+            assertEquals(bfs.checkConnectivty(), false);
+        } catch (IOException | IllegalVertexException | IllegalWeightException e) {
+            System.out.println("Niepoprawny plik");
+        }
+    } 
+}
